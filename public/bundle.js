4834f8f227e57d7e85f17c877c87dc86fea01409
Espen Grimsgaard <espen@r8edge.no>
2016-07-14 00:50:45 +0200

    Update todos on firebase

--- public/bundle.js
+++ public/bundle.js
@@ -27519,7 +27519,7 @@
 	    return React.createElement(
 	      'div',
 	      { className: todoClassName, onClick: function onClick() {
-	          dispatch(actions.toggleTodo(id));
+	          dispatch(actions.startToggleTodo(id, !completed));
 	        } },
 	      React.createElement(
 	        'div',
@@ -41603,7 +41603,7 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	exports.toggleTodo = exports.addTodos = exports.startAddTodo = exports.addTodo = exports.toggleShowCompleted = exports.setSearchText = undefined;
+	exports.startToggleTodo = exports.updateTodo = exports.addTodos = exports.startAddTodo = exports.addTodo = exports.toggleShowCompleted = exports.setSearchText = undefined;
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
@@ -41662,10 +41662,24 @@
 	  };
 	};
 
-	var toggleTodo = exports.toggleTodo = function toggleTodo(id) {
+	var updateTodo = exports.updateTodo = function updateTodo(id, updates) {
 	  return {
-	    type: 'TOGGLE_TODO',
-	    id: id
+	    type: 'UPDATE_TODO',
+	    id: id,
+	    updates: updates
+	  };
+	};
+
+	var startToggleTodo = exports.startToggleTodo = function startToggleTodo(id, completed) {
+	  return function (dispatch, getState) {
+	    var todoRef = _firebase.firebaseRef.child('todos/' + id);
+	    var updates = {
+	      completed: completed,
+	      completedAt: completed ? (0, _moment2.default)().unix() : null
+	    };
+	    return todoRef.update(updates).then(function () {
+	      dispatch(updateTodo(id, updates));
+	    });
 	  };
 	};
 
@@ -55807,15 +55821,10 @@
 	  switch (action.type) {
 	    case 'ADD_TODO':
 	      return [].concat(_toConsumableArray(state), [action.todo]);
-	    case 'TOGGLE_TODO':
+	    case 'UPDATE_TODO':
 	      return state.map(function (todo) {
 	        if (todo.id === action.id) {
-	          var nextCompleted = !todo.completed;
-
-	          return _extends({}, todo, {
-	            completed: nextCompleted,
-	            completedAt: nextCompleted ? moment().unix() : undefined
-	          });
+	          return _extends({}, todo, action.updates);
 	        } else {
 	          return todo;
 	        }
